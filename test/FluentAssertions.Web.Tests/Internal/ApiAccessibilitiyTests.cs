10877b01f381f4efc312ba27bc5a22f2cd04aa1c
Adrian Iftode <adrian@eplatforms.com>
2019-10-20 21:38:11 +0300

    Add a test to ensure classes designed for internal use do not become public
    Make AssertionsFailures and AssertionsFailures internal

---
+++ test/FluentAssertions.Web.Tests/Internal/ApiAccessibilitiyTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions.Execution;
+using System.Linq;
+using Xunit;
+
+namespace FluentAssertions.Web.Tests.Internal
+{
+    public class ApiAccessibilitiyTests
+    {
+        [Fact]
+        public void GivenTypesFromTheInternalNamespace_ThenShouldBeInternalOrLessThanInternal()
+        {
+            // Arrange
+            var typesFromInternal = typeof(HttpResponseMessageAssertions).Assembly
+                .GetTypes()
+                .Where(c => c.Namespace.Contains("Internal"));
+
+            // Assert
+            using (new AssertionScope())
+                foreach (var type in typesFromInternal)
+                {
+                    type.Should().NotHaveAccessModifier(Common.CSharpAccessModifier.Public);
+                }
+        }
+    }
+}
