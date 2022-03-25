da0aa1389e3db0b2d6b45c2b63c06a49b4dc1991
Adrian Iftode <adrian.iftode@google.com>
2022-03-26 00:41:30 +0200

    Simplify FluentAssertionsWebConfig locs

--- src/FluentAssertions.Web/FluentAssertionsWebConfig.cs
+++ src/FluentAssertions.Web/FluentAssertionsWebConfig.cs
@@ -1,7 +1,7 @@
-﻿// ReSharper disable once CheckNamespace
-using FluentAssertions.Web.Internal.Serializers;
+﻿using FluentAssertions.Web.Internal.Serializers;
 using System;
 
+// ReSharper disable once CheckNamespace
 namespace FluentAssertions
 {
     /// <summary>
@@ -9,31 +9,18 @@ namespace FluentAssertions
     /// </summary>
     public static class FluentAssertionsWebConfig
     {
-        private static ISerializer? serializer;
-        static FluentAssertionsWebConfig()
-        {
-            Serializer = new SystemTextJsonSerializer();
-        }
+        private static ISerializer? _serializer;
+
+        static FluentAssertionsWebConfig() => Serializer = new SystemTextJsonSerializer();
 
         /// <summary>
         /// The serializer instance used to deserialize the responses into a model of a specified typed
         /// </summary>
         public static ISerializer Serializer
         {
-            get
-            {
-                if (serializer == null)
-                {
-                    throw new InvalidOperationException("Serializer cannot be null");
-                }    
+            get => _serializer ?? throw new InvalidOperationException("Serializer cannot be null");
 
-                return serializer;
-            }
-
-            set
-            {
-                serializer = value ?? throw new ArgumentNullException(nameof(value), "Serializer cannot be null.");
-            }
+            set => _serializer = value ?? throw new ArgumentNullException(nameof(value), "Serializer cannot be null.");
         }
     }
 }
